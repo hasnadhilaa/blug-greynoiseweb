14a159d5d673833776c4d16fd4e28499eb533657
naomisiahaan <naomiylasiahaan@gmail.com>
2025-11-21 19:54:36 +0700

    menambahkan footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 
+
 export default function App() {
   return (
     <Router>
