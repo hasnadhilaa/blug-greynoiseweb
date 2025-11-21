f6f46283ed2b4a74c74f501e2989407a9ae00eb7
hasnadhilaa <hasnadila0@gmail.com>
2025-11-21 20:22:27 +0700

    Add route footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import OurTeam from "./pages/OurTeams";
 import Blog from "./pages/Blog";
+import Footer from "./components/Footer";
 
 export default function App() {
   return (
@@ -14,7 +15,9 @@ export default function App() {
         <Route path="/our-team" element={<OurTeam />} />
         <Route path="/blog" element={<Blog />} />
       </Routes>
-      
+
+      <Footer /> 
     </Router>
+
   );
 }
